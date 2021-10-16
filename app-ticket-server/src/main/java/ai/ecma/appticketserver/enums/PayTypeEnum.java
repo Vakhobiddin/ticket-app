2c4fb9328c35aa249ef1a06170aec957d912011e
Vakhobiddin <vboboqulov@mail.ru>
2021-10-16 11:21:31 -0700

    ticket app project

---
+++ app-ticket-server/src/main/java/ai/ecma/appticketserver/enums/PayTypeEnum.java
@@ -0,0 +1,12 @@
+package ai.ecma.appticketserver.enums;
+
+public enum PayTypeEnum {
+    CASH("Naqd pul"),
+    TRANSFER("Pul o'tkazmasi"),
+    CARD("Karta orqali");
+    public String nameUz;
+
+    PayTypeEnum(String nameUz) {
+        this.nameUz = nameUz;
+    }
+}
